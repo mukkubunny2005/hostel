6f7aac6f2eb7c15a1d39bb9310050abec4ea78cc
MUKKU BUNNY <mukkubunny2005@gmail.com>
2026-01-01 22:01:27 +0530

    chore: remove all files

--- core/encryption.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-import os
-KEY_FILE = "core/log.key"
-def load_chipper():
-    if not os.path.exists(KEY_FILE):
-        with open(KEY_FILE, "wb") as f:
-            f.write(Fernet.generate_key())
-    return Fernet(open(KEY_FILE, "rb").read())
