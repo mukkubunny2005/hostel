dcfc876c6afbb11a92b99bb9e38e11584cd0576d
MUKKU BUNNY <mukkubunny2005@gmail.com>
2026-01-02 22:31:43 +0530

    Add project files

--- models/auth_models.py
+++ models/auth_models.py
@@ -1,5 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field
-from typing import Optional
+from pydantic import BaseModel, Field
 
 # class UserCreate(BaseModel):
 # 	username: str = Field(..., min_length=3, max_length=50)
