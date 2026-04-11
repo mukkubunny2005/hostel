e1db37aa42918e4daeab37cb76fb627280ef16df
mukkubunny2005 <mukkubunny2005@gmail.com>
2026-04-11 15:32:20 +0530

    Update hostel_registration.py

--- routers/hostel_registration.py
+++ routers/hostel_registration.py
@@ -1,21 +1,21 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Form
-from sqlalchemy.orm import Session
-from database.session import get_db
-from typing import Annotated
-from models.hostel_registration_models import *
 import uuid
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+
+from database.session import get_db
+from models.hostel_registration_models import HostelRegistrationRequest
 from services import hostel_registration_services as hostel_services
 
 router = APIRouter()
 
+
 @router.post('/', response_model=HostelRegistrationRequest,
     status_code=status.HTTP_201_CREATED)
 def hostel_registration_from(db: Annotated[Session, Depends(get_db)], hostel_id: uuid.uuid5, owner_id: str, hostels:str = bool):
-    if hostels:
-        owner_id_ = owner_id
-    else:
-        owner_id_ = uuid.uuid5
+    owner_id_ = owner_id if hostels and owner_id else str(uuid.uuid4())
     try:
         hostel_form = hostel_services.create_hostel(db=db, hostel_id=hostel_id, owner_id=owner_id_)
         return {"msg": "hostel created", "hostel_id": hostel_form.hostel_id, "owner_id" : hostel_form.owner_id}
@@ -25,6 +25,3 @@ def hostel_registration_from(db: Annotated[Session, Depends(get_db)], hostel_id:
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Unable to create user at this time"
         )
-
-
-    
