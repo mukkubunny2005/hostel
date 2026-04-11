1664ff7aef290c8a48d331f8e190bd34531b9be6
mukkubunny2005 <mukkubunny2005@gmail.com>
2026-04-11 16:08:08 +0530

    Update tenant_registration.py

--- routers/tenant_registration.py
+++ routers/tenant_registration.py
@@ -1,16 +1,19 @@
+import uuid
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from services import tenant_registration_Services as tenant_services
-from models.tenant_registration_models import *
-from database.session import get_db
-from typing import Annotated
-router = APIRouter()
-import uuid
 from sqlalchemy.exc import SQLAlchemyError
+
+from database.session import get_db
+from models.tenant_Registration_models import TenantCreate
+from services import tenant_registration_Services as tenant_services
+
+router = APIRouter()
 
 
 @router.post('/tenant_registration', response_model=TenantCreate)
-async def tenant_registration_form(db: Annotated[Session, Depends(get_db)], hostel_id:str , tenant_id:str = uuid.uuid5):
+async def tenant_registration_form(db: Annotated[Session, Depends(get_db)], hostel_id:str, tenant_create: TenantCreate, tenant_id:str = uuid.uuid5):
     try:
         tenant_form = tenant_services.create_tenant(db=db, tenant_id=tenant_id, hostel_id=hostel_id)
         return {"message": "Tenant created", "tenant_id": tenant_id, "hostel_id":hostel_id}
